a507de9028259acf166645a9221c1d5b8edf5bb8
santhosh <santhosh44.sk44@gmail.com>
2024-05-21 10:51:31 +0530

    message added

---
+++ 01-hello-world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# shebang is the first line in shell-script,it is used to check the
+# syntax of our shell script,we need to give the location of bash
+# /bin/bash
+
+echo "hello shell scrpit"
