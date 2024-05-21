1823f731320562662d23dd44adb3a0e9d6117393
santhosh <santhosh44.sk44@gmail.com>
2024-05-21 11:34:10 +0530

    changed varibles.sh file

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh:: Hello Suresh, Good Morning"
-echo "Suresh:: Hi Ramesh,vey Good Morning"
-echo "Ramesh:: How are you Suresh?"
-echo "Suresh:: I am good Ramesh, How are you"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2, Good Morning"
+echo "$PERSON2:: Hi $PERSON1,vey Good Morning"
+echo "$PERSON1:: How are you $PERSON2?"
+echo "$PERSON2:: I am good $PERSON1, How are you"
